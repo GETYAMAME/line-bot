730b607763fca0e5410f7a8668ff2ca3e299fe50
GETYAMAME <pon.mikan10@gmail.com>
2023-03-28 11:13:39 +0900

    feat:openAiとの疎通完了

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,28 @@ import { Configuration, OpenAIApi } from "openai";
 config({ path: resolve(__dirname, "../.env") });
 const app = express();
 
+// LINEの設定値
 const client = new Client({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 });
+
+// OpenAIの設定値
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// OpenAIへの接続
+export async function ask(content: string, model = "gpt-3.5-turbo-0301") {
+  const response = await openai.createChatCompletion({
+    model: model,
+    messages: [{ role: "user", content: content }],
+  });
+
+  const answer = response.data.choices[0].message?.content;
+  return answer;
+}
 
 const port = process.env.PORT || 3000;
 
@@ -18,7 +36,6 @@ app.use(express.json());
 
 app.post("/callback", (req, res) => {
   console.log(req.body);
-
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((error) => {
@@ -27,12 +44,13 @@ app.post("/callback", (req, res) => {
     });
 });
 
-function handleEvent(event: any): Promise<any> {
+async function handleEvent(event: any): Promise<any> {
   if (event.type === "message" && event.message.type === "text") {
     const message: string = event.message.text;
+    const answer = await ask(message);
     const textMessage: TextMessage = {
       type: "text",
-      text: "Hello, world!",
+      text: answer as unknown as string,
     };
 
     return client.replyMessage(event.replyToken, textMessage);
